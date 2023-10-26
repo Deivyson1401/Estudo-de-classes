942161bcc21bdea80ab5279da46e79d30afc75a0
Deivyson José <deivyson.jd.silva@gmail.com>
2023-10-26 09:45:37 -0300

    iniciando

---
+++ Class.ts
@@ -0,0 +1,18 @@
+class pessoa {
+    nome: string;
+    idade: number;
+    CPF: string
+    constructor(
+        nome: string,
+        idade: number,
+        CPF: string
+    ){
+        this.nome = nome,
+        this.idade = idade,
+        this.CPF = CPF
+    }
+}
+
+class pessoa1 extends pessoa {
+    nom
+}
